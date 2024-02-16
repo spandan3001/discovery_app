050ae8fe9aecf11cc5d23baaef69810a2b254e85
spandanmn <tyian3001@gmail.com>
2024-02-17 02:05:07 +0530

    fix:ui changes

--- lib/views/discovery_page.dart
+++ lib/views/discovery_page.dart
@@ -25,40 +25,64 @@ class _DiscoveryPageState extends State<DiscoveryPage> {
     return Scaffold(
       // App Bar with the title "Discovery Page"
       appBar: AppBar(
-        title: const Text('Discovery Page'),
+        backgroundColor: Colors.red,
+        title: const Text(
+          'Discovery Page',
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        centerTitle: true,
       ),
       // Obx widget listens for changes in the ApiController's items and isLoading variables
       body: Obx(() {
-        return ListView.builder(
-          // Total number of items includes existing items and a loading indicator
-          itemCount: apiController.items.length + 1,
-          controller: apiController.scrollController,
-          itemBuilder: (context, index) {
-            // If index is within the range of existing items, display a Card with item details
-            if (index < apiController.items.length) {
-              return Card(
-                child: ListTile(
-                  title: Text(apiController.items[index].title),
-                  subtitle: Column(
-                    children: [
-                      Text(apiController.items[index].description),
-                      // Display image only if the URL is not empty
-                      if (apiController.items[index].imageUrl.isNotEmpty)
-                        Image.network(apiController.items[index].imageUrl),
-                    ],
+        return Center(
+          child: ListView.builder(
+            // Total number of items includes existing items and a loading indicator
+            itemCount: apiController.items.length + 1,
+            controller: apiController.scrollController,
+            itemBuilder: (context, index) {
+              // If index is within the range of existing items, display a Card with item details
+              if (index < apiController.items.length) {
+                return Card(
+                  elevation: 5.0,
+                  child: ListTile(
+                    title: Text(
+                      apiController.items[index].title,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    subtitle: Column(
+                      children: [
+                        Text(apiController.items[index].description),
+
+                        const SizedBox(height: 10),
+
+                        // Display image only if the URL is not empty
+                        if (apiController.items[index].imageUrl.isNotEmpty)
+                          SizedBox(
+                            width: double.maxFinite,
+                            height: 200,
+                            child: Image.network(
+                              apiController.items[index].imageUrl,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                      ],
+                    ),
                   ),
-                ),
-              );
-            } else {
-              // If index is beyond existing items, show loading indicator if data is still loading
-              if (apiController.isLoading.value) {
-                return const Center(child: CircularProgressIndicator());
+                ).paddingAll(8.0);
               } else {
-                // Otherwise, display an empty container to indicate the end of the list
-                return Container();
+                // If index is beyond existing items, show loading indicator if data is still loading
+                if (apiController.isLoading.value) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  // Otherwise, display an empty container to indicate the end of the list
+                  return Container();
+                }
               }
-            }
-          },
+            },
+          ),
         );
       }),
     );
